a557fbd49aa1a3dcbe0818df7c51b43ac0f73fac
pavel <tolpawsta@gmail.com>
2021-04-01 10:15:50 +0300

    Create StarbuzzCoffeeApp project

---
+++ Decorator/StarbuzzCoffeeApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StarbuzzCoffeeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
