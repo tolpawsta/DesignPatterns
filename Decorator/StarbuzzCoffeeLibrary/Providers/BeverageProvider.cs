dca16dede0ed26347f43a9d37cc59db485d5e6c8
pavel <tolpawsta@gmail.com>
2021-04-01 13:04:14 +0300

    Rename class Program to StarbuzzCoffee

--- Decorator/StarbuzzCoffeeLibrary/Providers/BeverageProvider.cs
+++ Decorator/StarbuzzCoffeeLibrary/Providers/BeverageProvider.cs
@@ -1,7 +1,18 @@
+using System.Collections.Generic;
+using StarbuzzCoffeeLibrary.Interfaces;
+using StarbuzzCoffeeLibrary.Models.Beverage;
+
 namespace StarbuzzCoffeeLibrary.Providers
 {
     public class BeverageProvider
     {
-        
+        public static List<IBeverage> GetBeverages()
+        {
+            return new List<IBeverage>()
+            {
+                new Espresso() { Cost = 1.60M },
+                new HouseBlend() { Cost = 2.0M }
+            };
+        }
     }
 }
