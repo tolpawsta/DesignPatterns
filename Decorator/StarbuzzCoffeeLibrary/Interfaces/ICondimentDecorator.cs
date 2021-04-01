32f565a76f84769fca5c691717a921efd68819ff
pavel <tolpawsta@gmail.com>
2021-04-01 10:35:47 +0300

    Create ICondimentDecorator interface

---
+++ Decorator/StarbuzzCoffeeLibrary/Interfaces/ICondimentDecorator.cs
@@ -0,0 +1,4 @@
+namespace StarbuzzCoffeeLibrary.Interfaces
+{
+    public interface ICondimentDecorator : IBeverage { }
+}
---
+++ Decorator/StarbuzzCoffeeLibrary/Models/Beverage/Espresso.cs
@@ -0,0 +1,7 @@
+namespace StarbuzzCoffeeLibrary.Models.Beverage
+{
+    public class Espresso
+    {
+        
+    }
+}
