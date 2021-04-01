16698499b7fcb79137c9d96e2edf7e7e2b0fad49
pavel <tolpawsta@gmail.com>
2021-04-01 13:29:29 +0300

    Realize Beverages and add condiment decorators

--- Decorator/StarbuzzCoffeeApp/StarbuzzCoffee.cs
+++ Decorator/StarbuzzCoffeeApp/StarbuzzCoffee.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using StarbuzzCoffeeLibrary.Models.CondimentDecorators;
+using StarbuzzCoffeeLibrary.Providers;
 
 namespace StarbuzzCoffeeApp
 {
@@ -6,6 +9,22 @@ namespace StarbuzzCoffeeApp
     {
         static void Main(string[] args)
         {
+            var view = new ConsoleView();
+            var beverages = BeverageProvider.GetBeverages();
+
+            // create beverage
+            var beverage = beverages.Take(1).FirstOrDefault();
+
+            // add CondimentDecorators
+            // add Mocha
+            beverage = new Mocha(beverage);
+            beverage = new Mocha(beverage);
+
+            // add Soy
+            beverage = new Soy(beverage);
+
+            // add Whip
+            beverage = new Whip(beverage);
         }
     }
 }
